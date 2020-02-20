879d27a9f381e8e42c3b04634109f3c86f16acda
Emmanuel <emmanuel.roberson3@gmail.com>
2020-02-20 17:38:51 -0600

    Working on implementing custom spring physics

---
+++ Assets/Scripts/Objects/JointDataObject.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "F")]
+public class JointDataObject
+{
+    public float Spring;
+    public float Damper;
+    public float MinDistance;
+    public float MaxDistane;
+    public float Tolerance;
+}
