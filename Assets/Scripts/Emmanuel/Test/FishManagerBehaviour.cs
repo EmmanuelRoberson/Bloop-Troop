b131c9629a353a1339cf047442f05c38f79b6203
Emmanuel <emmanuel.roberson3@gmail.com>
2020-03-10 10:20:50 -0500

    Added springs to fish school, working on managing each fish

---
+++ Assets/Scripts/Emmanuel/Test/FishManagerBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishManagerBehaviour : MonoBehaviour
+{
+    //list of the fish in the school
+    public List<GameObject> schoolAgents;
+
+    //list of each fish's collider
+    private List<Collider> schoolAgentColliders;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
