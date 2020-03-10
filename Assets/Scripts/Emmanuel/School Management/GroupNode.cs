b063149b991e1bcb46e1b6ac4b9c07fe4a16ac42
Emmanuel <emmanuel.roberson3@gmail.com>
2020-03-10 18:03:15 -0500

    Added a behaviour for testing collecting of fish

--- Assets/Scripts/Emmanuel/School Management/GroupNode.cs
+++ Assets/Scripts/Emmanuel/School Management/GroupNode.cs
@@ -12,4 +12,5 @@ public struct GroupNode
     
     public List<GroupNode> children;
     public List<GroupNode> parents;
+    
 }
---
+++ Assets/Scripts/Emmanuel/TestCollectBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCollectBehaviour : MonoBehaviour
+{
+    public int NumberOfRows;
+    
+    private SpringJoint sj;
+    public GameObject obj;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        sj = GetComponent<SpringJoint>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        
+    }
+}
