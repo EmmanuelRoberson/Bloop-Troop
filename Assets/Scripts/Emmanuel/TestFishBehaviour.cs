c9c4235040c30bb3de14327bffc5fea564d0aa08
Emmanuel Roberson <emmanuel.roberson3@gmail.com>
2020-04-01 23:20:34 -0500

    Collecting Fish now works, albeit very rudimentary
    Collecting fish now works, the next step is to integrate it in with the chain system, and after that, working on transformations

--- Assets/Scripts/Emmanuel/TestFishBehaviour.cs
+++ Assets/Scripts/Emmanuel/TestFishBehaviour.cs
@@ -1,50 +1,81 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using Emmanuel;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UIElements;
 
-public class TestFishBehaviour : MonoBehaviour
+namespace Emmanuel
 {
-    private GameObject parentFish;
-    
-    private SpringJoint springJoint;
-    private bool SpringConnected = false;
-
-    public bool NotInPosition
+    public enum FISH_STATE
     {
-        get { return (Vector3.Distance(transform.position, fishEnd.transform.position) > 0.005); }
+        UNCOLLECTED = 0,
+        COLLECTED = 1,
+
     }
-    
 
-    public Vector3 fishStart;
-    public GameObject fishEnd;
 
-    public float speed = 1.0f;
-    public float journeyLength = 1.0f;
-    private float startTime;
-
-    // Start is called before the first frame update
-    void Start()
+    public class TestFishBehaviour : MonoBehaviour
     {
-        //startTime = Time.time;
-        /*
+        public bool isPlayer;
+
+        public int fishAttached;
         
-        */
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        public GameObject nextSchoolPosition;
+        public GameObject nextSchoolPositionTwo;
         
-    }
+        public FISH_STATE state;
+        
+        private GameObject parentFish;
+    
+        private SpringJoint springJoint;
+        public bool SpringConnected = false;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (SpringConnected == false)
+        public bool NotInPosition
         {
+            get { return (Vector3.Distance(transform.position, fishEnd.transform.position) > 0.005); }
+        }
+    
+        
+        //where the fish will end up
+        public GameObject fishEnd;
+
+        public float speed = 1.0f;
+        public float journeyLength = 1.0f;
+        private float startTime;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (isPlayer)
+            {
+                state = FISH_STATE.COLLECTED;
+            }
+            else
+            {
+                state = FISH_STATE.UNCOLLECTED;
+            }
+            
+            fishAttached = 0;
+            SpringConnected = false;
+
+            //System.Random rand1 = new System.Random((int)Time.time);
+            //Vector3 offset = new Vector3(rand1.Next(-5, 5), rand1.Next(-5, 5), 0);
+
+            //nextSchoolPosition.transform.position += offset;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            TestFishBehaviour testFishBeh = other.gameObject.GetComponent<TestFishBehaviour>();
             TestPlayerBehaviour testBeh = other.gameObject.GetComponent<TestPlayerBehaviour>();
+            
             if (testBeh != null)
             {
                 parentFish = other.gameObject;
@@ -52,31 +83,86 @@ public class TestFishBehaviour : MonoBehaviour
 
                 fishEnd = testBeh.nextSchoolPosition;
                 ConnectSpring(fishEnd);
+
+                state = FISH_STATE.COLLECTED;
+            }
+
+            if (testFishBeh != null)
+            {
+                Debug.Log("fish behaviour not null" + name);
+                //if im collected
+                if (state == FISH_STATE.COLLECTED)
+                {
+                    Debug.Log("i am collected" + name);
+                    if (fishAttached == 0)
+                    {
+                        Debug.Log("I have no fish attached to me" + name);
+                        //if the other fish is uncollected
+                        if (testFishBeh.state == FISH_STATE.UNCOLLECTED)
+                        {
+                            Debug.Log("the fish i want to collect is uncollected" + name);
+                            //collect the other fish
+                            testFishBeh.state = FISH_STATE.COLLECTED;
+
+                            //make his ending the position after me
+                            testFishBeh.fishEnd = nextSchoolPosition;
+
+                            //connect his spring to his ending position
+                            testFishBeh.ConnectSpring(testFishBeh.fishEnd);
+                            
+                            //increase the number of fish attached
+                            fishAttached = 1;
+                            
+                            Debug.Log(fishAttached);
+                            return;
+                        }
+                    }
+                    
+                    if (fishAttached == 1)
+                    {
+                        //if the other fish is uncollected
+                        if (testFishBeh.state == FISH_STATE.UNCOLLECTED)
+                        {
+                            Debug.Log("ITS DUO TIME" + name);
+                            //collect the other fish
+                            testFishBeh.state = FISH_STATE.COLLECTED;
+
+                            //make his ending the other position after me
+                            testFishBeh.fishEnd = nextSchoolPositionTwo;
+
+                            //connect his spring to his ending position
+                            testFishBeh.ConnectSpring(testFishBeh.fishEnd);
+
+                            //increase the number of fish attached
+                            fishAttached = 2;
+                        }
+                    }
+                }
             }
         }
-    }
 
-    private void ConnectSpring(GameObject go)
-    {
-        if (!SpringConnected)
+        public void ConnectSpring(GameObject go)
         {
-            transform.position = go.transform.position;
+            if (!SpringConnected)
+            {
+                transform.position = go.transform.position;
             
-            SpringConnected = true;
+                SpringConnected = true;
             
-            springJoint = gameObject.AddComponent<SpringJoint>();
-            springJoint.anchor = Vector3.zero;
-            springJoint.connectedBody = go.GetComponent<Rigidbody>();
-            springJoint.connectedAnchor = go.transform.position;
-            springJoint.spring = 200;
-            springJoint.damper = 3;
-            springJoint.enableCollision = false;
-            springJoint.tolerance = 0.025f;
+                springJoint = gameObject.AddComponent<SpringJoint>();
+                springJoint.anchor = Vector3.zero;
+                springJoint.connectedBody = go.GetComponent<Rigidbody>();
+                springJoint.connectedAnchor = go.transform.position;
+                springJoint.spring = 300;
+                springJoint.damper = 10;
+                springJoint.enableCollision = false;
+                springJoint.tolerance = 0.025f;
+            }
         }
-    }
     
-    private IEnumerator JoinTheSchoolRoutine()
-    {
-        yield return null;
+        private IEnumerator JoinTheSchoolRoutine()
+        {
+            yield return null;
+        }
     }
 }
--- Assets/Scripts/Emmanuel/TestPlayerBehaviour.cs
+++ Assets/Scripts/Emmanuel/TestPlayerBehaviour.cs
@@ -1,27 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class TestPlayerBehaviour : MonoBehaviour
+namespace Emmanuel
 {
-    public GameObject nextSchoolPosition;
-    // Start is called before the first frame update
-    void Start()
+    public class TestPlayerBehaviour : MonoBehaviour
     {
-        System.Random rand1 = new System.Random((int)Time.time);
-        Vector3 offset = new Vector3(rand1.Next(-3, -1), rand1.Next(-3, -1), 0);
+        public GameObject nextSchoolPosition;
+        // Start is called before the first frame update
+        void Start()
+        {
+            System.Random rand1 = new System.Random((int)Time.time);
+            Vector3 offset = new Vector3(rand1.Next(-3, 3), rand1.Next(-3, 3 ), 0);
 
-        nextSchoolPosition.transform.position += offset;
-    }
+            nextSchoolPosition.transform.position += offset;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Update is called once per frame
+        void Update()
+        {
         
-    }
+        }
 
-    public void TestPlayerFunction()
-    {
-        Debug.Log("Testing Collision");
+        public void TestPlayerFunction()
+        {
+            Debug.Log("Testing Collision");
+        }
     }
 }
