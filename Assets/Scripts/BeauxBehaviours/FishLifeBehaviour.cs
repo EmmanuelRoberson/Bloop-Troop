35ed753b322f646185bcd60e7c5601c795e607c2
CompCod <beaux1rome@gmail.com>
2020-04-23 08:58:02 -0500

    game over
    added game over and a few extras, will be working with emmanuel on other things

--- Assets/Scripts/BeauxBehaviours/FishLifeBehaviour.cs
+++ Assets/Scripts/BeauxBehaviours/FishLifeBehaviour.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FishLifeBehaviour : MonoBehaviour
 {
@@ -9,7 +10,11 @@ public class FishLifeBehaviour : MonoBehaviour
 
     bool isDead = false;
 
-    public float iFrames = 0;
+    public float iFrameVal = 6;
+
+    float iFrames = 0;
+
+    public float countdown = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -22,24 +27,41 @@ public class FishLifeBehaviour : MonoBehaviour
         if(other.gameObject.CompareTag("Enemy") && iFrames <= 0)
         {
             healthVal -= 1;
-            iFrames = 6;
+            iFrames = iFrameVal;
+            transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y, transform.rotation.z + 0.4f, transform.rotation.w);
+        }
+
+        if (healthVal <= 0)
+        {
+            transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y, 3, transform.rotation.w);
+            isDead = true;
+            countdown = 3;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(healthVal<=0)
-        {
-            isDead = true;
-        }
 
         if (isDead == true)
         {
             GetComponentInParent<TestMovementBehaviour>().enabled = false;
+
+            countdown -= (Time.fixedDeltaTime);
+
+            if (countdown <= 0.0f)
+            {
+                SceneManager.LoadScene("GameOver");
+            }
+
         }
         else if(iFrames>0)
         {
+            if (iFrames <= iFrameVal-2)
+            {
+                transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y, 0, transform.rotation.w);
+            }
+
             iFrames -= (Time.fixedDeltaTime);
         }
     }
