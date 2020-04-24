56b2d6beb690c199e8ddcad04b662163b0d2f0d5
CompCod <beaux1rome@gmail.com>
2020-04-24 18:04:43 -0500

    Made win script work
    everything sucks rn, though

--- Assets/Scripts/BeauxBehaviours/WinConditionBehaviour.cs
+++ Assets/Scripts/BeauxBehaviours/WinConditionBehaviour.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WinConditionBehaviour : MonoBehaviour
 {
+    [SerializeField]
+    GameObject fish;
+
+    public float endOfLvl = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,9 @@ public class WinConditionBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (fish.transform.position.x >= endOfLvl)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
     }
 }
