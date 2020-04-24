1d2101d1a957fb55ae76420cca7e6aa57615d3e6
Emmanuel Roberson <emmanuel.roberson3@gmail.com>
2020-04-24 17:09:58 -0500

    Updated Spawner functionality, Added spawn point object
    Spawning now has a configurable chance to spawn fish at spawn points upon collision

--- Assets/Scripts/Emmanuel/Test/TestSpawnBehavoiur.cs
+++ Assets/Scripts/Emmanuel/Test/TestSpawnBehavoiur.cs
@@ -1,27 +1,41 @@
-﻿using Emmanuel;
+﻿using System;
+using Emmanuel;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class TestSpawnBehavoiur : MonoBehaviour
 {
-    public GameObject test;
-    public float timeToSpawn;
-    public float timer;
+    public GameObject ObjectToSpawn;
+    public float spawnChancePercent;
+
+    private Collider collider;
+    
     // Start is called before the first frame update
     void Start()
     {
-        timer = 0f;
+        collider = GetComponent<Collider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (timer > timeToSpawn)
+        
+    }
+
+    private void SpawnObject(Vector3 spawnPosition)
+    {
+        var obj = Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("SpawnPoint"))
         {
-            var obj = Instantiate(test, transform.position, Quaternion.identity);
-
-            timer = 0f;
+            var calculatedChance = Random.Range(0, 100);
+            if (calculatedChance <= spawnChancePercent)
+            {
+                SpawnObject(other.transform.position);
+            }
         }
-
-        timer += Time.deltaTime;
     }
 }
