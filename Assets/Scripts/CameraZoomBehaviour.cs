1d5cee3474dc5415b4a86c3d9bcd633ec05e04ba
emmanuel <emmanuel.roberson3@gmail.com>
2020-05-19 15:28:25 -0500

    Started on working on camera behaviour for collecting fish
    WOrking on blending values to represent a curve

---
+++ Assets/Scripts/CameraZoomBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoomBehaviour : MonoBehaviour
+{
+
+    public float a, b;
+
+
+    private float tDistance;
+    private float w;
+    public float currentValue;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        tDistance = Mathf.Abs(a - b);
+        w = currentValue / tDistance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentValue = (a * w) + (b * (1 - w));
+    }
+}
