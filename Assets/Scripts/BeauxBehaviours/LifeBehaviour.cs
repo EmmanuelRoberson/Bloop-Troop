3834cde55daa64da94fbe9c25fa32e5e0e3b00d4
CompCod <beaux1rome@gmail.com>
2020-06-19 15:29:29 -0500

    Life Behaviour changes

--- Assets/Scripts/BeauxBehaviours/LifeBehaviour.cs
+++ Assets/Scripts/BeauxBehaviours/LifeBehaviour.cs
@@ -33,15 +33,30 @@ public class LifeBehaviour : MonoBehaviour
     {
         if (isFish && iFrames <= 0)
         {
-            if (other.gameObject.CompareTag("Enemy") && iFrames <= 0 || other.gameObject.CompareTag("Parryable") && iFrames <= 0 && GetComponent<FishLifeBehaviour>().isParrying == false)
-            {
-                TakeDamage();
-                transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y, transform.rotation.z + 0.4f, transform.rotation.w);
+            //if (other.gameObject.CompareTag("Enemy") && iFrames <= 0 || other.gameObject.CompareTag("Parryable") && iFrames <= 0 && GetComponent<FishLifeBehaviour>().isParrying == false)
+            //{
+            //    TakeDamage();
+            //    transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y, transform.rotation.z + 0.4f, transform.rotation.w);
 
-                if (healthVal <= 0)
-                {
-                    GetComponent<FishLifeBehaviour>().isDead = true;
-                }
+            //    if (healthVal <= 0)
+            //    {
+            //        GetComponent<FishLifeBehaviour>().isDead = true;
+            //    }
+            //}
+
+            //Emmanuel code change 6/19/2020
+            if (other.CompareTag("Enemy") || other.CompareTag("Parryable"))
+            {
+                collider.enabled = false;
+                GameEvents.current.LoseFishEvent(GetComponent<SchoolFishBehaviour>());
+            }
+
+            CollectableFishBehaviour collectableFish = other.GetComponent<CollectableFishBehaviour>();
+            if (collectableFish != null)
+            {
+                GameEvents.current.CollectFishEvent(collectableFish.fishSprite);
+
+                Destroy(collectableFish.gameObject);
             }
         }
 
