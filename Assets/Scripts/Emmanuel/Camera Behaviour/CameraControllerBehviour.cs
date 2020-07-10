720e52e0a6f3c3cf266ca3c9eea710519885e372
emmanuel <emmanuel.roberson3@gmail.com>
2020-07-09 19:09:41 -0500

    Updated camera controller, updated scene

--- Assets/Scripts/Emmanuel/Camera Behaviour/CameraControllerBehviour.cs
+++ Assets/Scripts/Emmanuel/Camera Behaviour/CameraControllerBehviour.cs
@@ -7,34 +7,27 @@ public class CameraControllerBehviour : MonoBehaviour
     public Transform[] views;
     public float transitionSpeed;
     private Transform currentView;
+    private int viewsIndex;
     
     // Start is called before the first frame update
     void Start()
     {
+        viewsIndex = 0;
         currentView = views[0];
+        StartCoroutine(TransitionToViewRoutine());
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position =
-            Vector3.Lerp(transform.position, currentView.position, Time.fixedDeltaTime * transitionSpeed);
-
-        var currentViewRotation = currentView.transform.rotation;
         
-        Vector3 currentAngle = new Vector3(
-            Mathf.LerpAngle(transform.rotation.eulerAngles.x, currentViewRotation.eulerAngles.x, Time.deltaTime *transitionSpeed),
-            Mathf.LerpAngle(transform.rotation.eulerAngles.y, currentViewRotation.eulerAngles.y, Time.deltaTime *transitionSpeed),
-            Mathf.LerpAngle(transform.rotation.eulerAngles.z, currentViewRotation.eulerAngles.z, Time.deltaTime *transitionSpeed));
-
-
-        transform.eulerAngles = currentAngle;
-
+       
     }
 
-    private IEnumerator TransitionToViewRoutine(Transform viewTransform)
+    private IEnumerator TransitionToViewRoutine()
     {
-        while(Mathf.Abs(Vector3.Distance(transform.position, currentView.position)) > 0.1f)
+        Vector3 initialPosition = transform.position;
+        while(Mathf.Abs(Vector3.Distance(transform.position, currentView.position)) > 1f)
         {
             transform.position =
                 Vector3.Lerp(transform.position, currentView.position, Time.fixedDeltaTime * transitionSpeed);
@@ -42,15 +35,22 @@ public class CameraControllerBehviour : MonoBehaviour
             var currentViewRotation = currentView.transform.rotation;
         
             Vector3 currentAngle = new Vector3(
-                Mathf.LerpAngle(transform.rotation.eulerAngles.x, currentViewRotation.eulerAngles.x, Time.deltaTime *transitionSpeed),
-                Mathf.LerpAngle(transform.rotation.eulerAngles.y, currentViewRotation.eulerAngles.y, Time.deltaTime *transitionSpeed),
-                Mathf.LerpAngle(transform.rotation.eulerAngles.z, currentViewRotation.eulerAngles.z, Time.deltaTime *transitionSpeed));
+                Mathf.LerpAngle(transform.rotation.eulerAngles.x, currentViewRotation.eulerAngles.x, Time.fixedDeltaTime *transitionSpeed),
+                Mathf.LerpAngle(transform.rotation.eulerAngles.y, currentViewRotation.eulerAngles.y, Time.fixedDeltaTime *transitionSpeed),
+                Mathf.LerpAngle(transform.rotation.eulerAngles.z, currentViewRotation.eulerAngles.z, Time.fixedDeltaTime *transitionSpeed));
 
 
             transform.eulerAngles = currentAngle;
         
             yield return 0;   
         }
-       
+
+        viewsIndex++;
+        if (viewsIndex <= views.Length)
+        {
+            initialPosition = currentView.position;
+            currentView = views[viewsIndex];
+            
+        }
     }
 }
